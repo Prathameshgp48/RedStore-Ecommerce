5d14902f10d1782fde84ca02f38ee87556ffc1f9
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2025-01-05 12:47:07 +0530

    updated routes versions

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
 import adminRouter from "./routes/admin.routes.js"
 import orderRouter from "./routes/orders.routes.js"
+
 const app = express()
 
 app.use(
@@ -19,7 +20,7 @@ app.use(cookieParser());
 
 //route declaration
 app.use("/api/v1/users", userRouter)
-app.use("/api/v2/admin", adminRouter)
-app.use("/api/v3/orders", orderRouter)
+app.use("/api/v1/admin", adminRouter)
+app.use("/api/v1/orders", orderRouter)
 
 export { app };
